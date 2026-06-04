947633d083f4436fa2da8a4c13ff79399ef9e462
Ramirez-434 <ramirez@aluga.ai>
2026-06-04 13:04:31 -0300

    feat: Upgrade basemap to high-density OSM with Dark Mode CSS hack

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -259,11 +259,9 @@ export default function MapComponent({ properties, onPropertySelect, onPolygonFi
       >
           {/* Map tiles depending on theme */}
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
-            url={isDark 
-              ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-              : "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-            }
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            className={isDark ? "dark-map-tiles" : ""}
           />
           
           <MapSearchBar />
